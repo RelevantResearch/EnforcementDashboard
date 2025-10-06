3f36263b64a01c5227f6c7d4d76ae10a5924c92f
Michael-ale000 <michaelale85@gmail.com>
2025-10-06 14:59:43 +0545

    url patterns updated

--- arrestdashboard/urls.py
+++ arrestdashboard/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='home'),                  # Home page
+    path('ice-arrests/', views.dashboard, name='home'),                  # Home page
     path('documentation/', views.documentation, name='documentation'),  # Documentation page
 ]
